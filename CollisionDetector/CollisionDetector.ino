c398f14497262b28c25b360af56495ec9e957dbd
ContentsViewer <fivetwothreesix@gmail.com>
2018-10-29 02:28:45 +0900

    圧電ブザ昇圧しない方向にしたほうがいい

--- CollisionDetector/CollisionDetector.ino
+++ CollisionDetector/CollisionDetector.ino
@@ -15,20 +15,20 @@ void setup() {
 
     // tone(3, 880);
 
-    DDRD  |=  B00001000;
+    // DDRD  |=  B00001000;
 
 
-    TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); // 高速PWM
+    // TCCR2A = _BV(COM2B1) | _BV(WGM21) | _BV(WGM20); // 高速PWM
 
-    TCCR2B = _BV(CS21) |_BV(CS20) | _BV(WGM22);    // 分周1/64
+    // TCCR2B = _BV(CS21) |_BV(CS20) | _BV(WGM22);    // 分周1/64
 
-    OCR2A = 63;
-    OCR2B = 0;
+    // OCR2A = 63;
+    // OCR2B = 0;
 
-    //タイマー割り込み開始
-    TIMSK2 |= _BV(TOIE2);
+    // //タイマー割り込み開始
+    // //TIMSK2 |= _BV(TOIE2);
 
-
+    tone(4, 4000);
 }
 
 void loop() {
@@ -40,33 +40,33 @@ void loop() {
     // digitalWrite(4, HIGH);
     //delay(1000);
 
-    Serial.println(waveToggle);
+    // Serial.println(waveToggle);
 }
 
 ISR(TIMER2_OVF_vect){
+ 
+    // // High
+    // if(waveToggle){
+    //     waveToggle ^= 0x01;
 
-    // High
-    if(waveToggle){
-        waveToggle ^= 0x01;
 
+    //     OCR2A = 6;
+    //     OCR2B = 2;
+    // }
 
-        OCR2A = 6;
-        OCR2B = 2;
-    }
-
-    // Low
-    else {
-        pulseCount++;
-        if(pulseCount > 10){
-            pulseCount = 0;
+    // // Low
+    // else {
+    //     pulseCount++;
+    //     if(pulseCount > 10){
+    //         pulseCount = 0;
             
                     
-            OCR2A = 63;
-            OCR2B = 63;
+    //         OCR2A = 63;
+    //         OCR2B = 63;
 
-            waveToggle ^= 0x01;
-        }
-    }
+    //         waveToggle ^= 0x01;
+    //     }
+    // }
 
 }
 
